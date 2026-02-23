ae81a6ebb752f42e7647215d85d9c6239cb97e12
mmtol <mmigueltolosana@gmail.com>
2026-02-23 11:52:26 +0100

    cifrado txt basico

---
+++ MvcNetCoreUtilidades/Controllers/CifradosController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreUtilidades.Helpers;
+
+namespace MvcNetCoreUtilidades.Controllers
+{
+    public class CifradosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CifradoBasico()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CifradoBasico(string contenido, string resultado, string accion)
+        {
+            //ciframos el contenido
+            string response = HelperCriptography.EncriptarTxtBasico(contenido);
+
+            if (accion.ToLower() == "cifrar")
+            {
+                ViewData["Cifrado"] = response;
+            }
+            else if (accion.ToLower() == "comparar")
+            {
+                //si se quiere comprarar
+                //nos envia el txt en el resultado
+                if (response != resultado)
+                {
+                    ViewData["Comparacion"] = "Los datos no coinciden :(";
+                }
+                else
+                {
+                    ViewData["Comparacion"] = "Los datos coinciden, bieeeeeen";
+                }
+            }
+                return View();
+        }
+    }
+}
---
+++ MvcNetCoreUtilidades/Helpers/HelperCriptography.cs
@@ -0,0 +1,80 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MvcNetCoreUtilidades.Helpers
+{
+    public class HelperCriptography
+    {
+        //creamos un string para el salt
+        public static string Salt { get; set; }
+
+        //creamos los metodos static
+
+        //metodo para generar un salt
+        private static string GenerateSalt()
+        {
+            Random random = new Random();
+            string salt = "";
+            for (int i  = 1; i <= 30; ++i)
+            {
+                //generamo un aleatorio
+                int num = random.Next(1, 255);
+                char letra = Convert.ToChar(num);
+                salt += letra;
+            }
+
+            return salt;
+        }
+
+        //creamos un metodo eficiente para el cifrado
+        public static string EncriptarTxtBasicoSalt(string contenido, bool comparar)
+        {
+            if (comparar == false)
+            {
+                //no queremos comparar, solo cifrar
+                //creamos salt
+                Salt = GenerateSalt();
+            }
+
+            //realizamos el cifrado
+            string contenidoSalt = contenido + Salt;
+            //utilizamos el obj grande para cifrar
+            byte[] salida;
+            SHA512 managed = SHA512.Create();
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            salida = encoding.GetBytes(contenido);
+            //realizar n iteraciones sobre el propio cifrado
+            for (int i = 1; i <= 50; ++i)
+            {
+                //cifrado sobre cifrado
+                salida = managed.ComputeHash(salida);
+            }
+            //debemos liberar la memoria
+            managed.Clear();
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+
+        //simplemente vamos a devolver un texto cifrado
+        public static string EncriptarTxtBasico(string contenido)
+        {
+            //el cifrado se realiza a nivel de bytes
+            //debemos convertir el txt de entrada a bytes[]
+            byte[] entrada;
+            //despues de cifrar los bytes, os dará una salida de bytes[]
+            byte[] salida;
+            //necesitamos una clase para convertir de byte a strng y viceversa
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            //necesitamos un obj para cifrar el contenido
+            SHA1 managed = SHA1.Create();
+            //convertimos el txt a bytes
+            entrada = encoding.GetBytes(contenido);
+            //los objetos de cifrado tienen un metodo llamado ComputerHash()
+            //que recibe un byte[], realizan acciones internas y devuelven el byte[] cifrado
+            salida = managed.ComputeHash(entrada);
+            //convertimos los bytes[] a txt
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+    }
+}
